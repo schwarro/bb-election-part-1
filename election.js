60ae2030b1ba3fde87841f2b0222d11c2cf52d29
schwarro <rob.ap.schwartz@gmail.com>
2018-09-12 15:18:15 -0400

    completed part 1

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
-
+  $.ajax({
+    url: "https://bb-election-api.herokuapp.com/",
+    method: "GET",
+    data: {},
+    dataType: "json"
+  }).done(function(responseData){
+    var candidates = responseData.candidates;
+    for (var i = 0; i < candidates.length; i++) {
+      var li = $("<li>");
+      li.html(candidates[i].name + ": " + candidates[i].votes + " votes");
+      $(".candidates").append(li);
+    }
+  });
 });
